7bbe18e4b7f20896026aeae07969dff019dabd2f
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-07-01 11:26:34 +0900

    add empty functions

--- denops/paiza-io/main.ts
+++ denops/paiza-io/main.ts
@@ -1,4 +1,16 @@
 import { Denops } from "https://deno.land/x/denops_std@v1.0.0-alpha.0/mod.ts";
 
+async function paizaIO() {
+}
+
+async function create() {
+}
+
+async function getStatus() {
+}
+
+async function getDetails() {
+}
+
 export async function main(denops: Denops): Promise<void> {
 }
