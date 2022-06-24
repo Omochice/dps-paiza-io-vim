b1fde3ccb2e21800b8218689b71a60c8a59edf85
GitHub <noreply@github.com>
2022-06-24 01:46:34 +0000

    :package: update deno dependencies

--- denops/paiza-io/main.ts
+++ denops/paiza-io/main.ts
@@ -1,6 +1,6 @@
-import { Denops } from "https://deno.land/x/denops_std@v3.3.1/mod.ts";
-import { execute } from "https://deno.land/x/denops_std@v3.3.1/helper/mod.ts";
-import * as vars from "https://deno.land/x/denops_std@v3.3.1/variable/mod.ts";
+import { Denops } from "https://deno.land/x/denops_std@v3.3.2/mod.ts";
+import { execute } from "https://deno.land/x/denops_std@v3.3.2/helper/mod.ts";
+import * as vars from "https://deno.land/x/denops_std@v3.3.2/variable/mod.ts";
 import {
   ensureArray,
   ensureNumber,
